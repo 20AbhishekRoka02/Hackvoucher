0af427cfbe99257f87e87eb219350a702ea12a15
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2024-11-02 00:07:49 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,17 @@
+// const mongoose = require('mongoose') ;
+/* Work is:-
+1. Create Cluster at MongoDB Atlas
+2. Create User and Password
+3. Add name of cluster
+4. set string to uri
+5. Connect it
+6. See it in test.
+*/
+const uri = 'mongodb+srv://<username>:<password>@cluster0.sakylkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 const mongoose = require('mongoose') ;
 
-mongoose.connect('mongodb+srv://nisaar:Nidsar@cluster0.ztmue.mongodb.net/Hackvoucher_collection?retryWrites=true&w=majority') ;
+mongoose.connect(uri) ;
  const db = mongoose.connection ;
 
  db.on('error' , console.error.bind(console , "error connecting to db")) ;
